65211213fcbe79d0b67922466b9de1f9d5d52f0e
demo <jhsansom@umich.edu>
2024-02-21 10:00:58 -0500

    Added validation script

--- build_docker.sh
+++ build_docker.sh
@@ -8,6 +8,8 @@ sleep 10
 cp ./single_file-1.22.42.xpi ./WebTop/Desktop/single_file.xpi & echo "Installing SingleFile installer"
 cp ./listener-plugin/record.sh ./WebTop/Desktop/record2.sh & echo "Installing recorder"
 cp ./replay-tool/replay.sh ./WebTop/Desktop/replay2.sh & echo "Installing replay tool"
+cp ./replay-tool/validate.sh ./WebTop/Desktop/validate2.sh & echo "Installing validation tool"
 mv ./WebTop/Desktop/record2.sh ./WebTop/Desktop/record.sh & echo "Moving recorder"
 mv ./WebTop/Desktop/replay2.sh ./WebTop/Desktop/replay.sh & echo "Moving replay"
+mv ./WebTop/Desktop/validate2.sh ./WebTop/Desktop/validate.sh & echo "Moving validate"
 echo "Done copying files"
