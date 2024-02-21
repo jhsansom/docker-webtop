7b3f97466d7162d3ddd5e446a9c23e47ae1ce31d
demo <jhsansom@umich.edu>
2024-02-20 21:03:32 -0500

    Removed copy command that did nothing

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -6,7 +6,6 @@ if [ -f "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml ]; then
     "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
 fi
 sudo mv /usr/lib/python3.11/EXTERNALLY-MANAGED /usr/lib/python3.11/EXTERNALLY-MANAGED.bak
-sudo cd /etc/replay-tool && pip install -r requirements.txt
 pip
 #cd /etc/listener-plugin && web-ext run &
 /usr/bin/xfce4-session > /dev/null 2>&1
