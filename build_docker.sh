f5d0cdfbcf958ba7a58bc685b8b83f560092c98d
demo <jhsansom@umich.edu>
2024-03-07 11:04:25 -0500

    Added parental control protections

--- build_docker.sh
+++ build_docker.sh
@@ -4,8 +4,10 @@ cp -r ../../../replay-tool ./replay-tool
 docker build .
 docker compose up -d
 wget wget https://addons.mozilla.org/firefox/downloads/file/4235927/single_file-1.22.42.xpi
+wget wget https://addons.mozilla.org/firefox/downloads/file/3790346/foxfilter-9.6.xpi
 sleep 10
 cp ./single_file-1.22.42.xpi ./WebTop/Desktop/single_file.xpi & echo "Installing SingleFile installer"
+cp ./foxfilter-9.6.xpi ./WebTop/Desktop/foxfilter.xpi & echo "Installing SingleFile installer"
 cp ./listener-plugin/record.sh ./WebTop/Desktop/record2.sh & echo "Installing recorder"
 cp ./replay-tool/replay.sh ./WebTop/Desktop/replay2.sh & echo "Installing replay tool"
 cp ./replay-tool/validate.sh ./WebTop/Desktop/validate2.sh & echo "Installing validation tool"
