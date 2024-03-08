7d08b83264822f3cb58a32c0f6ba04fdfbe2e12e
Jake Sansom <jhsansom@umich.edu>
2024-03-08 22:40:02 +0000

    Added port tracking

--- build_docker.sh
+++ build_docker.sh
@@ -8,6 +8,7 @@ wget wget https://addons.mozilla.org/firefox/downloads/file/3790346/foxfilter-9.
 sleep 10
 cp ./single_file-1.22.42.xpi ./WebTop/Desktop/single_file.xpi & echo "Installing SingleFile installer"
 cp ./foxfilter-9.6.xpi ./WebTop/Desktop/foxfilter.xpi & echo "Installing SingleFile installer"
+cp ./port.txt ./WebTop/port.txt & echo "Copying port.txt"
 cp ./listener-plugin/record.sh ./WebTop/Desktop/record.sh & echo "Installing recorder"
 cp ./replay-tool/replay.sh ./WebTop/Desktop/replay.sh & echo "Installing replay tool"
 cp ./replay-tool/validate.sh ./WebTop/Desktop/validate.sh & echo "Installing validation tool"
